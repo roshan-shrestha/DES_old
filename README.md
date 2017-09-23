bbe04e44c67d56eb205f786ef81e720354b63813
rs285 <rs1646@truman.edu>
2017-09-23 13:59:45 -0500

    key input

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DES
 Implementation of DES algorithm
 
-CS 455 - Computer Security
+CS 455 - Computer Security: Project 1
+
 Instructor: Dr. Chetan Jaiswal, [Truman State University](http://www.truman.edu)
--- main.cpp
+++ main.cpp
@@ -3,14 +3,25 @@
 
 using namespace std;
 
+
+//bitset<64> charToBits(const char pt[8])
+//{
+//    bitset<64>;
+//    for
+//}
+
 int main()
 {
-    string read, write;
+    string read, write, key;
     ifstream in_file("input.txt");
+    ifstream key_file("key_file.txt");
     ofstream out_file("output.txt");
 
     getline(in_file, read);
     cout << read << endl;
+
+    getline(key_file, key);
+    cout << "this is the key: " << key;
 
     write = "This file was created inside a C++ program.";
     out_file << write << endl;
