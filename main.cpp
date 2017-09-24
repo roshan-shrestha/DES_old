5d437154780677f10185800b851c596fb4c88e49
Nischal Shrestha <nischalsht7442@gmail.com>
2017-09-23 21:08:17 -0500

    temp function

--- main.cpp
+++ main.cpp
@@ -5,13 +5,40 @@
 
 using namespace std;
 
-int main()
+void tempfunction(string read)
 {
-    string read, write, key;
     int count = 0;
     int asci;
     string binary;
     string eight_binary;
+
+    vector<char> split_array(read.begin(),read.end());
+
+    // Access each character in the file ( each char can be accessed by *i)
+    for (vector<char>::const_iterator i = split_array.begin(); i != split_array.end(); ++i)
+    {
+        count++;
+        //cout << "count is: "<< count << endl;
+        asci = int(*i);
+        binary = bitset<8>(asci).to_string();
+
+        cout << binary << ' ' << endl;
+        eight_binary += binary;
+
+        if ((count % 8) == 0)
+        {
+            cout << eight_binary << endl;
+            eight_binary.clear();
+        }
+
+
+    }
+}
+
+int main()
+{
+    string read, write, key;
+
     ifstream in_file("input.txt");
     ifstream key_file("key_file.txt");
     ofstream out_file("output.txt");
@@ -25,23 +52,7 @@ int main()
     write = "This file was created inside a C++ program.";
     out_file << write << endl;
 
-    vector<char> split_array(read.begin(),read.end());
+    tempfunction(read);
 
-    // Access each character in the file ( each char can be accessed by *i)
-    for (vector<char>::const_iterator i = split_array.begin(); i != split_array.end(); ++i)
-    {
-        count++;
-        //cout << "count is: "<< count << endl;
-        asci = int(*i);
-        binary = bitset<8>(asci).to_string();
-        eight_binary += binary;
-        if ((count % 8) == 0)
-        {
-            cout << eight_binary << endl;
-            eight_binary.clear();
-        }
-        
-
-    }
     return 0;
 }
